4509173001e9eda2c8c190517f0cd726135b0d7a
Tithi Raval <114683834+traval0@users.noreply.github.com>
2024-04-24 14:57:42 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from dotenv import load_dotenv
+import os
+import requests
+
+
+load_dotenv()
+
+CARBON_INTERFACE_API_KEY = os.getenv('CARBON_INTERFACE_API_KEY')
+
+app = Flask(__name__)
+
+# routing URL on website to a home page
+@app.route('/')   
+def index():
+    return 'This is the home page'
+
+
+if __name__ == "__main__":   # We only start the webserver when this file is called directly
+    app.run(debug=True)   # This starts the app
