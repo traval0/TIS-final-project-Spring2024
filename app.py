5fbd2feaf214a9184cbbb46d4ccd6aa228871aca
Tithi Raval <traval0@chicagobooth.edu>
2024-05-06 01:46:48 -0500

    Got login and registration functionality working

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
-from flask import Flask, request, render_template, flash, redirect
+from flask import Flask, request, render_template, flash, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy 
 from flask_migrate import Migrate
-from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
+from flask_login import login_user, LoginManager, login_required, logout_user, current_user
+from models import db, User
 from forms import LoginForm, RegisterForm
 from dotenv import load_dotenv
 import os
 import requests
-
+from flask_bcrypt import Bcrypt
 
 load_dotenv()
 
@@ -14,20 +15,25 @@ CARBON_INTERFACE_API_KEY = os.getenv('CARBON_INTERFACE_API_KEY')
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
-db = SQLAlchemy(app) 
+db.init_app(app)  # Initialize db with the app
 migrate = Migrate(app, db)
+bcrypt = Bcrypt(app)
 app.config['SECRET_KEY'] = 'thisisasecretkey'
-
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), nullable=False, unique=True)
-    password = db.Column(db.String(80), nullable=False)
 
 with app.app_context():
     db.create_all()
 
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
 # routing URL on website to a login & signup page
-# features - login, signup
 @app.route('/')   
 @app.route('/<user>')   # can have multiple routes to the same function
 def index(user=None):
@@ -37,28 +43,30 @@ def index(user=None):
 @app.route('/login', methods=['GET', 'POST'])   
 def login():
     form = LoginForm()
-    print(form.validate_on_submit())
+    # print(form.validate_on_submit())
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user:
+            if bcrypt.check_password_hash(user.password, form.password.data):
+                login_user(user)
+                return redirect(url_for('dashboard'))
     return render_template('login.html', form=form)
 
-@app.route('/register')   
+@app.route('/register', methods=['GET', 'POST'])   
 def register():
     form = RegisterForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data)
+        new_user = User(username=form.username.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
 
-# routing URL on website to a home page
 # features - dashboard, calculator, log
 @app.route('/home')   
 def home():
     return 'This is the home page with dashboard, calculator, & log'
-
-@app.route('/test', methods=['GET', 'POST'])   # need to specify POST because default is GET
-def test():
-    if request.method == 'POST':
-        return 'This is a POST request'
-    else:
-        return 'This is a GET request'
-    #return f'Method used: {request.method}'
-    #return 'This is the home page with dashboard, calculator, & log'
 
 # routing URL on website to a profile page
 @app.route('/myprofile')   
@@ -71,9 +79,16 @@ def profile(username):
     return render_template("profile.html", username=username)
 
 # routing URL on website to a dashboard
-@app.route('/dashboard')   
+@app.route('/dashboard', methods=['GET', 'POST'])   
+@login_required
 def dashboard():
-    return 'This is my dashboard'
+    return render_template("dashboard.html", username=current_user.username)
+
+@app.route('/logout', methods=['GET', 'POST'])
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 def calculate_carbon_footprint(CARBON_INTERFACE_API_KEY, activity_type, activity_value):
     url = "https://www.carboninterface.com/api/v1/estimates"
--- forms.py
+++ forms.py
@@ -1,16 +1,23 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, Length, ValidationError
+from models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=20)])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
 
 class RegisterForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('Password', validators=[InputRequired(), Length(min=4, max=20)])
     # confirm_password = PasswordField('Password', validators=[DataRequired()])
     # remember_me = BooleanField('Remember Me')
     submit = SubmitField('Register')
+    def validate_username(self, username):
+        existing_user_username = User.query.filter_by(
+            username=username.data).first()
+        if existing_user_username:
+            raise ValidationError(
+                'That username already exists. Please choose a different one.')
